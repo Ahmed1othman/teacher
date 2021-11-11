d7d250a8bf1bacec6c962d89791dec75a668fc6b
mastafa3alie@gmail.com <75615844+mastafa3aly@users.noreply.github.com>
2021-11-11 11:04:50 +0200

    public files

--- resources/views/auth/app.blade.php
+++ resources/views/auth/app.blade.php
@@ -9,9 +9,9 @@
     <link rel="stylesheet" href="{{ asset('font/iconsmind-s/css/iconsminds.css') }}" />
     <link rel="stylesheet" href="{{ asset('font/simple-line-icons/css/simple-line-icons.css') }}" />
 
-    <link rel="stylesheet" href="{{ asset('css/vendor/bootstrap.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/vendor/bootstrap.rtl.only.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/vendor/bootstrap-float-label.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/vendorAsset/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/vendorAsset/bootstrap.rtl.only.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/vendorAsset/bootstrap-float-label.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/main.css') }}" />
 </head>
 
@@ -22,8 +22,8 @@
             @yield('content')
         </div>
     </main>
-    <script src="{{ asset('js/vendor/jquery-3.3.1.min.js') }}"></script>
-    <script src="{{ asset('js/vendor/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/vendorAsset/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('js/vendorAsset/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('js/dore.script.js') }}"></script>
     <script src="{{ asset('js/scripts.js') }}"></script>
 </body>
